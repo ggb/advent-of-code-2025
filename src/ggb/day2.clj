59a0eee594ee415b77168057775876fdb2ff5c46
Gregor <ggb@informatik.uni-kiel.de>
2025-12-02 09:53:46 +0100

    some file removals and day2

--- src/ggb/day2.clj
+++ src/ggb/day2.clj
@@ -1,1 +1,33 @@
-(ns ggb.day2)
+(ns ggb.day2
+  (:require [clojure.string :as str]))
+
+(def input 
+  (->> (str/split (slurp "data/day2.txt") #",")
+       (map #(str/split % #"-"))
+       (map (fn [[fst snd]]
+              (range (parse-long fst) (inc (parse-double snd)))))
+       (reduce concat)
+       (map str)))
+
+;; Part 1
+(->> input
+     (filter #(even? (count %)))
+     (filter #(apply = (partition (/ (count %) 2) %)))
+     (map parse-long)
+     (reduce +))
+
+;; Part 2
+(defn valid?
+  [s]
+  (let [len (count s)]
+    (->> (range 1 len)
+         (filter #(= 0 (mod len %)))
+         (map #(partition % s))
+         (map #(apply = %))
+         (some true?))))
+
+(->> input
+     (map str)
+     (filter valid?)
+     (map parse-long)
+     (reduce +))
--- test/ggb/advent_of_code_2025_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ggb.advent-of-code-2025-test
-  (:require [clojure.test :refer :all]
-            [ggb.advent-of-code-2025 :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
