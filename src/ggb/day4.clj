8b3c285e711d1f3659bf5504ba1b11706d36c35e
Gregor <ggb@informatik.uni-kiel.de>
2025-12-04 11:15:06 +0100

    small improvements

--- src/ggb/day4.clj
+++ src/ggb/day4.clj
@@ -1,38 +1,35 @@
 (ns ggb.day2
   (:require [clojure.string :as str]))
 
-(def input 
+(def input
   (->> (slurp "data/day4.txt")
        str/split-lines
        (map seq)
        (map vec)
        vec))
 
-(defn get2d 
+(defn get2d
   [grid [x y]]
   (when (and (<= 0 x (dec (count grid)))
              (<= 0 y (dec (count (grid x)))))
     (get-in grid [x y])))
-
-(def directions 
-  [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]])
 
 (defn neighbors
   [grid [x y]]
   (map
    (fn [[dx dy]]
      (get2d grid [(+ x dx) (+ y dy)]))
-   directions))
+   [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]))
 
-(defn find-neighbors 
+(defn find-neighbors
   [grid]
   (for [x (range (count grid))
         y (range (count (grid x)))]
-    (when (= \@ (get2d grid [x y])) 
+    (when (= \@ (get2d grid [x y]))
       [[x y] (neighbors grid [x y])])))
 
 (defn less-than-4
-  [row] 
+  [row]
   (->> row
        second
        (map #(if (= \@ %) 1 0))
@@ -41,20 +38,20 @@
 
 (defn get-updates
   [grid]
-  (->> grid 
+  (->> grid
        find-neighbors
        (filter some?)
        (filter less-than-4)))
 
 ;; Part 1
-(->> input 
+(->> input
      get-updates
      count)
 
 ;; Part 2
-(defn update-grid 
+(defn update-grid
   [updates grid]
-  (reduce (fn [acc val] 
+  (reduce (fn [acc val]
             (assoc-in acc (first val) \.)) grid updates))
 
 (defn get-total-rolls
@@ -66,4 +63,3 @@
       (get-total-rolls (+ acc n-changes) (update-grid updates grid)))))
 
 (get-total-rolls 0 input)
-
